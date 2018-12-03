218890631fcbdd6b96eb5fd4b1ec26d1c9c990d6
Kevin J. Burns, P.E. <kevin.burns.pe@gmail.com>
2018-12-03 06:59:13 -0600

    Initial commit

---
+++ src/com/github/kjburns/pdfmarker/PdfMarkerMain.java
@@ -0,0 +1,16 @@
+package com.github.kjburns.pdfmarker;
+
+import javax.swing.SwingUtilities;
+
+/**
+ * The main for pdf-marker-poc
+ * @author Kevin J. Burns, P.E.
+ *
+ */
+public final class PdfMarkerMain {
+	public static void main(String[] args) {
+		SwingUtilities.invokeLater(() -> {
+			// TODO invoke something
+		});
+	}
+}
